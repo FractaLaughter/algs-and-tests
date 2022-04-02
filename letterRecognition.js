4941fec5617650d493332463f65fce58349342ea
Jason Stephenson <fractalaughter@gmail.com>
2022-04-02 19:19:25 -0400

    Improve control flow

--- letterRecognition.js
+++ letterRecognition.js
@@ -1,21 +1,22 @@
-let character = 'o';
+let character = 'l';
 let userGuesses = ['cold', 'fold', 'bath','','','']
 let solution = 'load';
 
 const getStatus = (character) => {  
-    if (userGuesses.toString().split('').includes(character)) {  
-        if (solution.split('').includes(character)) {            
-            for (let i = 0; i < userGuesses.length; i++) {
-                for (let j = 0; j < userGuesses[i].split('').length; j++) {
-                    if (character === userGuesses[i].split('')[j] && character === solution.split('')[j]) {
-                        return 'correct';
-                    }
-                }
-            }
-            return 'present';
-        }
-        return 'absent';
+    if (!userGuesses.toString().split('').includes(character)) {
+        return 'uneval';
     }
+    if (!solution.split('').includes(character)) {          
+        return 'absent';  
+    }
+    for (let i = 0; i < userGuesses.length; i++) {
+        for (let j = 0; j < userGuesses[i].split('').length; j++) {
+            if (character === userGuesses[i].split('')[j] && character === solution.split('')[j]) {
+                return 'correct';
+            }
+        }
+    }
+    return 'present';
 }
 
 console.log(getStatus(character));
