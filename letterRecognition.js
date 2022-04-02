e8f5b4e9d9f6252cf3cf660a1d385b1ab292dbaa
Jason Stephenson <fractalaughter@gmail.com>
2022-04-02 14:27:38 -0400

    Clean up unneccessary variables

--- letterRecognition.js
+++ letterRecognition.js
@@ -2,27 +2,20 @@ let character = 'o';
 let userGuesses = ['cold', 'fold', 'bath','','','']
 let solution = 'load';
 
-const getStatus = (character) => {
-    let evaluation = 'uneval'
-    let solutionArr = solution.split('')
-    
-    if (userGuesses.toString().split('').includes(character)) {    
-        if (solutionArr.includes(character)) {
+const getStatus = (character) => {  
+    if (userGuesses.toString().split('').includes(character)) {  
+        if (solution.split('').includes(character)) {            
             for (let i = 0; i < userGuesses.length; i++) {
-                let guess = userGuesses[i].split('');
-                for (let j = 0; j < guess.length; j++) {
-                    if (character === guess[j] && character === solutionArr[j]) {
-                        evaluation = 'correct';
-                    } else if (evaluation !== 'correct') {
-                        evaluation = 'present';
+                for (let j = 0; j < userGuesses[i].split('').length; j++) {
+                    if (character === userGuesses[i].split('')[j] && character === solution.split('')[j]) {
+                        return 'correct';
                     }
                 }
-            } return evaluation;
+            }
+            return 'present';
         }
-        else {
-            evaluation = 'absent';
-        }
-    } return evaluation;
+        return 'absent';
+    }
 }
 
 console.log(getStatus(character));
